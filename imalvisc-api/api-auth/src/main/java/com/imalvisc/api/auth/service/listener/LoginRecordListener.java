7ad076ef18fc351fecc0f0fd91ac0311ccfd88ca
ChenJiaMing <13570038865@163.com>
2019-01-27 01:30:48 +0800

    集成RabbitMQ环境

--- imalvisc-api/api-auth/src/main/java/com/imalvisc/api/auth/service/listener/LoginRecordListener.java
+++ imalvisc-api/api-auth/src/main/java/com/imalvisc/api/auth/service/listener/LoginRecordListener.java
@@ -1,4 +1,4 @@
-package com.imalvisc.api.auth.rabbitmq;
+package com.imalvisc.api.auth.service.listener;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
