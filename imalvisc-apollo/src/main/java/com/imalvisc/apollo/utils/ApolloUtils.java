c4dd795b273fa4794e5b32eec1811fb8c4431a57
imalvisc <13570038865@163.com>
2019-06-04 23:30:39 +0800

    完成apollo公共模块

---
+++ imalvisc-apollo/src/main/java/com/imalvisc/apollo/utils/ApolloUtils.java
@@ -0,0 +1,13 @@
+package com.imalvisc.apollo.utils;
+
+/**
+ * @author imalvisc
+ * @version v1.0
+ * @ClassName ApolloUtils
+ * @Description Apollo工具类
+ * @motto 学会编程而不是学会编码！
+ * @date 2019-06-04 23:30
+ * @Copyright Guangzhou CheXingYi Information Technology Co., Ltd.
+ */
+public class ApolloUtils {
+}
