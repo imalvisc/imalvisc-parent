13be4208b7ac762c86a4d14386bb79436e3b8634
imalvisc <13570038865@163.com>
2020-07-06 20:03:38 +0800

    希尔排序

---
+++ algorithm/src/main/java/com/imalvisc/algorithm/sort/ShellSort.java
@@ -0,0 +1,29 @@
+package com.imalvisc.algorithm.sort;
+
+import java.util.Arrays;
+
+/**
+ * 希尔排序算法
+ */
+public class ShellSort {
+
+    public static void main(String[] args) {
+
+        int[] arr = new int[]{1, -1, 15, 66, -3, 55, -8};
+        int insertVal;
+        int insertIdx;
+        for (int step = arr.length / 2; step > 0; step /= 2) {
+            for (int i = step; i < arr.length; i++) {
+                insertVal = arr[i];
+                insertIdx = i - step;
+                while (insertIdx >= 0 && insertVal < arr[insertIdx]) {
+                    arr[insertIdx + step] = arr[insertIdx];
+                    insertIdx -= step;
+                }
+                arr[insertIdx + step] = insertVal;
+            }
+        }
+        System.out.println("排序后结果：" + Arrays.toString(arr));
+    }
+
+}
